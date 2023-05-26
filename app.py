a00a44ae4399e09630e6797a3fd964f376506ff0
OumaymaDourrou <salamoumayma@gmail.com>
2023-05-26 11:06:42 +0200

    carico l'esame

---
+++ app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import sklearn as sk
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def main():
+    st.text("Ciao questo front-end funziona")
+
+if __name__ == "__main__":
+    main()
